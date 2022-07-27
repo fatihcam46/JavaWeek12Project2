2b15a80eddce7f97507ab24e2ebdab802c3b66a8
fatihcam46 <fatih.cam.bahar@gmail.com>
2022-07-27 18:32:47 +0100

    instance vs local example

--- src/fieldTypes/StaticMethods.java
+++ src/fieldTypes/StaticMethods.java
@@ -1,4 +1,21 @@
 package fieldTypes;
 
-public class StaticMethods {
-}
+            public class StaticMethods {
+                public int num=10;
+
+                public static int count = 5;
+
+                public static void displayMessage(String message){
+                    System.out.println("message = " + message);
+                    // System.out.println("num = " + num); non-static field,cannot write
+                    System.out.println("count = " + count);
+                    //  instanceMethod();  non-static field
+                }
+
+                public void instanceMethod(){
+                    System.out.println("instance method called");
+                    //  System.out.println("num = " + num);
+                    //  System.out.println("count = " + count);
+                    displayMessage("hello from instance method");
+                }
+            }
--- src/fieldTypes/StaticTest.java
+++ src/fieldTypes/StaticTest.java
@@ -1,4 +1,25 @@
 package fieldTypes;
 
 public class StaticTest {
+    public static void main(String[] args) {
+        //class name//object name//print>>
+        StaticMethods.displayMessage("Hello from static method");//message = Hello from static method
+
+        StaticMethods object = new StaticMethods();//count = 5
+
+        object.displayMessage("We are using object to call this static method");
+//message = We are using object to call this static method
+        System.out.println("StaticMethods.count = " + StaticMethods.count);//StaticMethods.count = 5
+
+        System.out.println("object.num = " + object.num);//object.num = 10
+
+        System.out.println(new StaticMethods().num);//10
+//    StaticMethods.instanceMethod();
+    }
 }
+
+
+
+
+
+
